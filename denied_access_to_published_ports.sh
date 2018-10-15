f4b17851e5a9da4ba9f23561cc940869f8aaccf4
Marek Ramotowski <ma-ra@users.noreply.github.com>
2018-10-15 05:57:54 -0400

    Denied/Access to shared/published ports by other containers on the docker host interfaces.

---
+++ denied_access_to_published_ports.sh
@@ -0,0 +1,21 @@
+awk '
+{
+    cmd = "docker inspect --format \"{{ .NetworkSettings.IPAddress }}\" openvpn"
+    if (cmd | getline > 0) { openvpn_container_ip=$0"/32" }
+    close(cmd)
+    
+    cmd = "iptables -t nat -S"
+    while (cmd | getline > 0) { 
+        if ($0 ~ /-A DOCKER.*! -i docker0.*DNAT --to-destination/) { 
+            gsub("! -i docker0","-s "openvpn_container_ip,$0)
+            gsub("-A DOCKER","iptables -t nat -D DOCKER",$0) 
+            print $0
+            cmd2=$0
+            if (cmd2 | getline > 0) { print $0 }
+            close(cmd2)
+        }
+    }
+    close (cmd)
+}
+' - <<< ""
+iptables -t nat -L -nv
